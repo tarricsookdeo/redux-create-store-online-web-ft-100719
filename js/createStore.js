89138d0740c04f3fe183be01d5cc13b1654f6690
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-02-29 23:23:20 -0500

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -8,21 +8,21 @@ function reducer(state = { count: 0 }, action) {
     default:
       return state;
   }
-};
+}
 
-function dispatch(action){
+function dispatch(action) {
   state = reducer(state, action);
   render();
-};
+}
 
 function render() {
   let container = document.getElementById('container');
   container.textContent = state.count;
-};
+}
 
-dispatch({ type: '@@INIT' })
+dispatch({ type: '@@INIT' });
 let button = document.getElementById('button');
 
 button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
-})
+  dispatch({ type: 'INCREASE_COUNT' });
+});
